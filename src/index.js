bf77700e4e8263a6a7c708dd628d9c8ddb751d76
REMEESH <remeesh97@gmail.com>
2023-10-25 12:31:23 +0530

    cred

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Navbar , Footer} from './pages/layout';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+
+function App(){
+  return(
+    <>
+    <BrowserRouter>
+    <Navbar/>
+
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/products' element={<Product/>} />
+
+     
+    </Routes>
+    <Footer/>
+
+    </BrowserRouter>
+   
+   
+=    </>
+   
+     
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-center mb-3'>
+        <h2 className='text-center mb-3'>Home Page</h2>
+        </div>
+  )
+}
+
+export default Home;
